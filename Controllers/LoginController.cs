f8e3cde4f357b19808de0e1538c161cf4356bbe9
avishle <avishle@ac.sce.ac.il>
2017-03-21 08:36:21 +0200

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using TLP.DAL;
+using TLP.Models;
+
+namespace TLP.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Login", new User());
+        }
+        public ActionResult Authenticate(User loginUser)
+        {
+            DataAccessLayer uDAL = new DataAccessLayer();
+            if (ModelState.IsValid)
+            {
+                List<User> userLogin = (from u in uDAL.Users
+                                        where (u.userName == loginUser.userName) &&
+                                               (u.password == loginUser.password)
+                                        select u).ToList<User>();
+                if (userLogin.Count == 1)
+                {
+                    FormsAuthentication.SetAuthCookie("loginCookie", true);
+                    return RedirectToRoute("Home");
+                }
+                return View("Login", loginUser);
+            }
+            return View("Login", loginUser);
+
+        }
+        
+        public ActionResult Registrate()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TLP.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public String userName { get; set; }
+        [Required]
+        public String password { get; set; }
+    }
+}
